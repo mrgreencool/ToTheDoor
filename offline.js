0cbb06698078db1cf849eec70a1e48f04b3b615d
mrgreencool <40354922+mrgreencool@users.noreply.github.com>
2018-06-18 18:00:25 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1529312081,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/tiledbackground3.png",
+		"images/sprite2-sheet0.png",
+		"images/tiledbackground5.png",
+		"images/door1-sheet0.png",
+		"images/player-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"images/sprite8-sheet0.png",
+		"images/tiledbackground2.png",
+		"images/tiledbackground7.png",
+		"images/door2-sheet0.png",
+		"images/door3-sheet0.png",
+		"images/up_touch-sheet0.png",
+		"images/door4-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"channel.html",
+		"cordova.oauth2.js"
+	]
+}
